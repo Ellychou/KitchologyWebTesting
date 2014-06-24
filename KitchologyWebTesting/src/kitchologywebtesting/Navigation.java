c9239be7887460bc2a230fbba787a6af574f9e2c
yanspeers <yanspeers@gmail.com>
2014-06-24 16:12:13 -0400

    Update from 6/24/2014
    Tuesdays update

--- KitchologyWebTesting/src/kitchologywebtesting/Navigation.java
+++ KitchologyWebTesting/src/kitchologywebtesting/Navigation.java
@@ -1,5 +1,9 @@
 package kitchologywebtesting;
 
+import java.lang.String;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -9,6 +13,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.collections.Lists;
 
 public class Navigation {
 
@@ -50,6 +55,7 @@ public class Navigation {
                 driver.findElement(By.linkText(tmpLink)).click();
                 //Wait for browser
                 Thread.sleep(2000);
+                //TODO: Use textSimilarity method to confirm link accuracy
             } catch (Exception e) {
                 //Print error and associated link
                 System.out.println(tmpLink);
@@ -92,6 +98,7 @@ public class Navigation {
                 driver.findElement(By.linkText(tmpLink)).click();
                 //Wait for browser
                 Thread.sleep(2000);
+                //TODO: Use textSimilarity method to confirm link accuracy
             } catch (Exception e) {
                 //Print error and associated link
                 System.out.println(tmpLink);
@@ -134,6 +141,7 @@ public class Navigation {
                 driver.findElement(By.linkText(tmpLink)).click();
                 //Wait for browser
                 Thread.sleep(2000);
+                //TODO: Use textSimilarity method to confirm link accuracy
             } catch (Exception e) {
                 //Print error and associated link
                 System.out.println(tmpLink);
@@ -142,5 +150,57 @@ public class Navigation {
         }
         //Close driver
         driver.close();
+    }
+
+    //Compares two imput strings and returns two if their similarity exceeds a certain threshold
+    public boolean textSimilarity(String s1, String s2) {
+
+        double thresh = 0.8;
+        int simTally = 0;
+
+        //remove pluralizations and possessives
+        s1 = s1.replace("'s", "");
+        s2 = s2.replace("'s", "");
+        s1 = s1.replace("s ", " ");
+        s2 = s2.replace("s ", " ");
+
+        //Split the strings into individual words
+        //then remove nonKeywords
+        String[] split1 = s1.split(" ");
+        String[] split2 = s2.split(" ");
+
+        split1 = removeNonKeywords(s1.split(" "));
+        split2 = removeNonKeywords(s2.split(" "));
+
+        //Compare all possible pairs of split1 and split2
+        for (String tmpString1 : split1) {
+            for (String tmpString2 : split2) {
+                //if the strings are equivalent, increment the similarity tally
+                if (tmpString1.equalsIgnoreCase(tmpString2)) {
+                    simTally++;
+                }
+            }
+        }
+
+        //if the number of equvalent words is > 80% of
+        if ((double) simTally / Math.min(split1.length, split2.length) > thresh) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String[] removeNonKeywords(String[] input) {
+        ArrayList<String> resultList = new ArrayList<String>();
+
+        for (String item : input) {
+            if (item.length() > 4) {
+                resultList.add(item);
+            }
+        }
+
+        String[] resultArray = new String[resultList.size()];
+        resultList.toArray(resultArray);
+        return resultArray;
     }
 }
