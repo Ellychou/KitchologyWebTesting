6b0a547b67b1af582decaea4ba82d3a87a7682d8
yanspeers <yanspeers@gmail.com>
2014-06-25 17:45:35 -0400

    Update for 6/25/2014
    Wednesdays update
    
    New test class implemented: Login
    
    Comments added throughout

--- KitchologyWebTesting/src/kitchologywebtesting/Login.java
+++ KitchologyWebTesting/src/kitchologywebtesting/Login.java
@@ -1,10 +1,95 @@
 package kitchologywebtesting;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
 public class Login {
 
-    private String[] credentials;
+    //Empty constructor
+    public Login() {
+    }
 
-    public Login(){
+    //Login via Chrome using the input credentials
+    public void loginAsChrome(String username, String password) {
+        //Instantiate Driver
+        WebDriver driver = new ChromeDriver();
 
+        try {
+            //navigate to the base url
+            driver.get(Main.baseURL);
+            //wait for the page to load
+            Thread.sleep(1000);
+            //locate and click the login link
+            driver.findElement(By.id("loginLink")).click();
+            //Find the name field and enter credentials
+            driver.findElement(By.id("name")).sendKeys(username);
+            //find the password field and enter credentials
+            driver.findElement(By.id("password")).sendKeys(password);
+            //find the button and cllick submit
+            driver.findElement(By.id("j_idt26")).click();
+            //wait for the browser to navigate to the new page
+            Thread.sleep(10000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //Close the driver, free memory
+        driver.close();
+    }
+
+    //Login via Firefox using the input credentials
+    public void loginAsFirefox(String username, String password) {
+        //Instantiate Driver
+        WebDriver driver = new FirefoxDriver();
+
+        try {
+            //navigate to the base url
+            driver.get(Main.baseURL);
+            //wait for the page to load
+            Thread.sleep(1000);
+            //locate and click the login link
+            driver.findElement(By.id("loginLink")).click();
+            //Find the name field and enter credentials
+            driver.findElement(By.id("name")).sendKeys(username);
+            //find the password field and enter credentials
+            driver.findElement(By.id("password")).sendKeys(password);
+            //find the button and cllick submit
+            driver.findElement(By.id("j_idt26")).click();
+            //wait for the browser to navigate to the new page
+            Thread.sleep(10000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //Close the driver, free memory
+        driver.close();
+    }
+
+    //Login via Internet Explorer using the input credentials
+    public void loginAsIE(String username, String password) {
+        //Instantiate Driver
+        WebDriver driver = new InternetExplorerDriver();
+
+        try {
+            //navigate to the base url
+            driver.get(Main.baseURL);
+            //wait for the page to load
+            Thread.sleep(1000);
+            //locate and click the login link
+            driver.findElement(By.id("loginLink")).click();
+            //Find the name field and enter credentials
+            driver.findElement(By.id("name")).sendKeys(username);
+            //find the password field and enter credentials
+            driver.findElement(By.id("password")).sendKeys(password);
+            //find the button and cllick submit
+            driver.findElement(By.id("j_idt26")).click();
+            //wait for the browser to navigate to the new page
+            Thread.sleep(10000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //Close the driver, free memory
+        driver.close();
     }
 }
--- KitchologyWebTesting/src/kitchologywebtesting/Main.java
+++ KitchologyWebTesting/src/kitchologywebtesting/Main.java
@@ -2,18 +2,37 @@ package kitchologywebtesting;
 
 public class Main {
 
+    //For now, the base URL will be stored as a global variable
+    //Accessible in all classes as *Main.baseURL*
+    public static final String baseURL = "http://sausage.kitchology.com:8080/kitchology/";
+
+    //Constructor (Main logic for this class)
+    //***CALL TEST METHODS HERE***
     public Main() {
+        //Identify the various driver.exe files
         locateDrivers();
-        Navigation nav = new Navigation("http://sausage.kitchology.com:8080/kitchology/");
+
+        //Initialize an instance of Navigation
+        Navigation nav = new Navigation();
+        //Initialize an instance of Login
+        Login log = new Login();
+        
+        //TODO: Access methods of nav ( nav.methodExample() )
+        //TODO: Access methods of log ( log.exampleMethod() )
+        
     }
 
+    //Identifies the location of all necesary webdriver.exe files
     private void locateDrivers(){
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
         System.setProperty("webdriver.ie.driver", "drivers/iedriver.exe");
         //TODO: SafariDriver
     }
 
+    //Main method
+    //***FLOW OF CONTROL BEGINS HERE***
     public static void main(String[] args) {
+        //Initialize a new instance of Main class (call constructor)
         Main m = new Main();
     }
 }
